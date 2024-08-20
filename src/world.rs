ee5625dac3339faf5232fe9c88d252243f4e797b
Johan Felisaz <johan.felisaz@epfl.ch>
2024-08-20 20:31:54 +0200

    crude world generation; nondeterministic for now

--- src/world.rs
+++ src/world.rs
@@ -8,6 +8,7 @@ use strum::IntoEnumIterator;
 use crate::block_kind::Block;
 use crate::block_kind::Block::{DIRT, GRASS};
 use crate::cube::Cube;
+use crate::world_generation::perlin::PerlinNoise;
 
 pub struct World {
     /// The list of the chunks currently being displayed
@@ -30,13 +31,13 @@ impl World {
         self.chunks.push(Chunk::new_for_demo([-2. * s, 0.], 0));
     }
 
-    /// Creates a new random world. For now this is a simple, flat
+    /// Creates a basic, flat world. For now this is a simple, flat
     /// grassland, extending `nchunks` in each direction.
     ///
     /// 'n_chunks': number of chunks the flat lands extends in any
     /// direction; i.e., (2nchunks + 1) x (2nchunks + 1) chunks will
     /// be created
-    pub fn create_new_random_world(n_chunks: i32) -> Self {
+    pub fn create_new_flat_world(n_chunks: i32) -> Self {
         let s = CHUNK_SIZE as f32;
         let mut chunks = vec![];
 
@@ -57,6 +58,53 @@ impl World {
         world
     }
 
+    /// Creates a simple world with hills. A single octave Perlin noise is used,
+    /// so don't expect anything fancy.
+    pub fn create_new_random_world(n_chunks: i32) -> Self {
+	let mut noise = PerlinNoise::new(42, 32.);
+
+        let s = CHUNK_SIZE as f32;
+        let mut chunks = vec![];
+	let terrain_scale = 20.;
+	let terrain_offset = CHUNK_FLOOR as f32;
+
+        // Yes this is slow, but it will be fine for now
+        for i in -n_chunks..n_chunks + 1 {
+            for j in -n_chunks..n_chunks + 1 {
+                let mut chunk = Chunk::new([i as f32 * s, j as f32 * s]);
+
+		// get the height from the perlin noise for each block
+		for x in 0..8 {
+		    for z in 0..8 {
+			let height = terrain_offset
+			    + terrain_scale * noise.at([i as f32 * s + x as f32,
+							j as f32 * s + z as f32]);
+
+			let cube_height = height.floor() as i32;
+
+			for y in 0..cube_height - 1 {
+			    chunk.set_cube(Vector3::new(i as f32 * s + x as f32,
+							y as f32,
+							j as f32 * s + z as f32),
+					   DIRT);
+			}
+			chunk.set_cube(Vector3::new(i as f32 * s + x as f32,
+						    cube_height as f32,
+						    j as f32 * s + z as f32),
+				       GRASS);
+		    }
+		}
+		
+                chunks.push(chunk);
+            }
+        }
+
+        let mut world = Self { chunks };
+        world.compute_visible_cubes();
+        world
+    }
+
+    
     /// Loads a world from a file.
     pub fn from_file(name: &str) -> Option<Self> {
         match std::fs::read_to_string(name) {
---
+++ src/world_generation.rs
@@ -0,0 +1,4 @@
+/// This module contains all code related to the world generation, from noise to
+/// terrain.
+
+pub mod perlin;
