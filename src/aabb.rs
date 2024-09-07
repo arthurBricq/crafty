61bfb32110de67c7eec2d1a84dedecb9905ff0a5
Johan Felisaz <johan.felisaz@epfl.ch>
2024-09-07 09:41:56 +0200

    remove displacement status class

--- src/aabb.rs
+++ src/aabb.rs
@@ -23,37 +23,6 @@ impl AABB {
     }
 }
 
-#[derive(Debug)]
-pub enum DisplacementStatus {
-    Still,
-    Forward,
-    Backward,
-}
-
-impl DisplacementStatus {
-    pub fn from_scalar(displacement: f32) -> DisplacementStatus {
-        if displacement < 0.
-        { Self::Backward } else if displacement > 0.
-        { Self::Forward } else { Self::Still }
-    }
-
-    pub fn to_scalar(&self) -> f32 {
-        match self {
-            Self::Still => 0.,
-            Self::Forward => 1.,
-            Self::Backward => -1.,
-        }
-    }
-
-    pub fn from_vector(displacement: Vector3) -> [DisplacementStatus; 3] {
-        [
-            Self::from_scalar(displacement[0]),
-            Self::from_scalar(displacement[1]),
-            Self::from_scalar(displacement[2])
-        ]
-    }
-}
-
 #[cfg(test)]
 mod tests {
     use crate::aabb::*;
