296e8a83900a72411524c6c45071c8c6f03160ac
Johan Felisaz <johan.felisaz@epfl.ch>
2024-09-07 18:55:28 +0200

    crude crafting ui

--- src/graphics/inventory_menu.rs
+++ src/graphics/inventory_menu.rs
@@ -1,4 +1,4 @@
-use crate::graphics::color::Color::LightGray;
+use crate::graphics::color::Color::{LighterGray, LightGray};
 use crate::graphics::rectangle::RectInstance;
 
 pub struct InventoryMenu {
@@ -18,7 +18,41 @@ impl InventoryMenu {
                                           2. - 2. * MARGIN_W,
                                           2. - 2. * MARGIN_H,
                                           LightGray));
-        
+
+        // we want a 4 rows * 8 columns item grid for the inventory, and item
+        // bar
+        let inner_margin = 0.05;
+        let startx = -1. + MARGIN_W + inner_margin;
+        // because margin taken into account by item slot
+        let endx = 1. - MARGIN_W;
+
+        let starty = -1. + MARGIN_H + inner_margin;
+        let endy = 1. - MARGIN_H - 1.; // same
+
+        let nrows = 4;
+        let ncolumns = 8;
+
+        let slotw_with_margin = (endx - startx) / ncolumns as f32;
+        let sloth_with_margin = (endy - starty) / nrows as f32;
+
+        for row in 0..nrows {
+            for column in 0..ncolumns {
+                rects.push(
+                    RectInstance::new_from_corner(startx + column as f32 * slotw_with_margin,
+                                                  starty + row as f32 * sloth_with_margin,
+                                                  slotw_with_margin - inner_margin,
+                                                  sloth_with_margin - inner_margin,
+                                                  LighterGray));
+            }
+        }
+
+        // crafting menu
+        rects.push(
+            RectInstance::new_from_corner(-1. + MARGIN_W + 0.9,
+                                          -1. + MARGIN_H + 0.9,
+                                          0.7, 0.7,
+                                          LighterGray));
+
         Self {
             rects: rects
         }
