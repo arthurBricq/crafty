73cb91ec79db5a80ece8a082751a9c92504ea21c
Arnaud <ar.tan@hotmail.fr>
2024-09-07 11:17:26 +0200

    Correct bug #29 Now only head have a pitch rotation

--- src/graphics/entity.rs
+++ src/graphics/entity.rs
@@ -76,17 +76,40 @@ impl EntityCube {
         }
     }
 
+    /// Build a rendered cube without pitch rotation
+    pub fn new_only_yaw(position: &Position, body_part_id: u8, scale: [f32; 3]) -> Self {
+        Self { 
+            world_matrix: Self::model_matrix_rot_y(position, scale),
+            // body part_id correspond to the [6*body_part_id,6*body_part_id+5] texture loaded
+            body_part_id,
+        }
+    }
+
     /// Generate a world matrix with a scaing over each direction
     /// a rotation around y then
-    /// a rotation around local x
+    /// a rotation around local x then
+    /// a translation
     fn model_matrix_rot_yx(position: &Position, scale: [f32; 3]) -> [[f32; 4]; 4] {
         let yaw = position.yaw();
         let pitch = position.pitch();
         [
             [  scale[0] * yaw.cos() * pitch.cos(), scale[0] *pitch.sin(),  scale[0] * yaw.sin() * pitch.cos(), 0.],
             [ -scale[1] * yaw.cos() * pitch.sin(), scale[1] *pitch.cos(), -scale[1] * yaw.sin() * pitch.sin(), 0.],
-            [ -scale[2] * yaw.sin()              , 0.0                  ,  scale[2] * yaw.cos()              , 0.],
-            [position.x()                        , position.y()         , position.z()                       , 1.]
+            [               -scale[2] * yaw.sin(),                   0.0,                scale[2] * yaw.cos(), 0.],
+            [                        position.x(),          position.y(),                        position.z(), 1.]
+        ]
+    }
+
+    /// Generate a world matrix with a scaing over each direction
+    /// a rotation around y then
+    /// a translation
+    fn model_matrix_rot_y(position: &Position, scale: [f32; 3]) -> [[f32; 4]; 4] {
+        let yaw = position.yaw();
+        [
+            [  scale[0] * yaw.cos(),           0.,  scale[0] * yaw.sin(), 0.],
+            [                    0.,     scale[1],                    0., 0.],
+            [ -scale[2] * yaw.sin(),           0.,  scale[2] * yaw.cos(), 0.],
+            [          position.x(), position.y(),          position.z(), 1.]
         ]
     }
 }
