8c30fca3832fce834e2263087c5fce5c337b2e0c
Marin Bricq <mbricq@logitech.com>
2024-09-07 12:44:46 +0200

    Updated server side

--- src/main_server.rs
+++ src/main_server.rs
@@ -1,8 +1,8 @@
-use crafty::game_server::GameServer;
+use crafty::args::Args;
 use crafty::network::tcp_server::TcpServer;
 use crafty::world::World;
+use crafty::{game_server::GameServer, world_generation::world_generator::WorldGenerator};
 use std::sync::{Arc, Mutex};
-use crafty::args::Args;
 
 #[allow(dead_code)]
 enum WorldInitializer {
@@ -14,14 +14,14 @@ enum WorldInitializer {
 fn main() {
     let args = Args::from_args();
     let url = args.url();
-    
+
     // Create the initial world
     let init = WorldInitializer::FLAT;
     println!("[Server] Creating a world ...");
     let world = match init {
-        WorldInitializer::RANDOM => World::create_new_random_world(10),
-        WorldInitializer::FLAT => World::create_new_flat_world(10),
-        WorldInitializer::DISK => World::from_file("map.json").unwrap()
+        WorldInitializer::RANDOM => WorldGenerator::create_new_random_world(10),
+        WorldInitializer::FLAT => WorldGenerator::create_new_flat_world(10),
+        WorldInitializer::DISK => World::from_file("map.json").unwrap(),
     };
     println!("                          ... Finished !");
 
