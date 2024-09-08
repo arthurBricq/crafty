c0ad46684feafe67d83ff667e6250df20c4d3294
Johan Felisaz <johan.felisaz@epfl.ch>
2024-09-08 13:09:43 +0200

    working crafts

--- src/graphics/inventory_menu.rs
+++ src/graphics/inventory_menu.rs
@@ -119,6 +119,8 @@ impl InventoryMenu {
     }
 
     fn handle_button_pressed(&mut self) -> UpdateStatus {
+        let mut update_craft = false;
+        
         if let Some(carried_item) = self.carried_item {
             // if we have a valid slot, put it in
 
@@ -128,7 +130,6 @@ impl InventoryMenu {
             for row in 0..INVENTORY_NROWS {
                 for col in 0..INVENTORY_NCOLS {
                     if self.inventory_slots[row][col].is_in(&self.cursor_pos) {
-                        // grab it
                         if row == 0 {
                             // for item bar
                             if self.items.put_bar_item(col, carried_item) {
@@ -137,9 +138,23 @@ impl InventoryMenu {
                         }
                         else {
                             // for inventory
-                            if self.items.put_inventory_item((row - 1) * INVENTORY_NCOLS + col, carried_item) {
+                            if self.items.put_inventory_item((row - 1) * INVENTORY_NCOLS + col,
+                                                             carried_item) {
                                 self.carried_item = None;
                             }
+                        }
+                    }
+                }
+            }
+
+            // also check for crafting slots
+            for row in 0..3 {
+                for col in 0..3 {
+                    if self.crafting_slots[row][col].is_in(&self.cursor_pos) {
+                        if let None = self.crafting_items[row][col] {
+                            std::mem::swap(&mut self.crafting_items[row][col],
+                                           &mut self.carried_item);
+                            update_craft = true;
                         }
                     }
                 }
@@ -166,7 +181,37 @@ impl InventoryMenu {
                     }
                 }
             }
+
+            // also check for crafting slots
+            for row in 0..3 {
+                for col in 0..3 {
+                    if self.crafting_slots[row][col].is_in(&self.cursor_pos) {
+                        if let Some(_) = self.crafting_items[row][col] {
+                            std::mem::swap(&mut self.crafting_items[row][col],
+                                           &mut self.carried_item);
+                            update_craft = true;
+                        }
+                    }
+                }
+            }
+
+            // and for the crafting output
+            if self.crafting_output_slot.is_in(&self.cursor_pos) {
+                if let Some(_) = self.crafting_output_items {
+                    std::mem::swap(&mut self.crafting_output_items,
+                                   &mut self.carried_item);
+                    // also remove the crafting components !
+                    for row in 0..3 {
+                        for col in 0..3 {
+                            let _ = &mut self.crafting_items[row][col].take();
+                        }
+                    }
+                    update_craft = true;
+                }
+            }
         }
+
+        if update_craft { self.update_craft(); }
 
         self.update();
         UpdateStatus::Update
@@ -217,9 +262,10 @@ impl InventoryMenu {
                     let slot = InventorySlot::new(InventoryPosition::new(craftx + col as f32 * (item_size + margin),
                                                                          crafty + row as f32 * (item_size + margin)),
                                                   item_size);
-
                     self.crafting_slots[row][col] = slot;
-                    self.rects.append(&mut slot.rects(&self.ui_rect, None, slot.is_in(&self.cursor_pos)));
+
+                    let item = self.crafting_items[row][col].map(|block| { (block, 1) });
+                    self.rects.append(&mut slot.rects(&self.ui_rect, item, slot.is_in(&self.cursor_pos)));
                 }
             }
 
@@ -229,7 +275,9 @@ impl InventoryMenu {
                                                                      crafty + 1 as f32 * (item_size + margin)),
                                               item_size);
                 self.crafting_output_slot = slot;
-                self.rects.append(&mut slot.rects(&self.ui_rect, None, slot.is_in(&self.cursor_pos)));
+
+                let item = self.crafting_output_items.map(|block| { (block, 1) });
+                self.rects.append(&mut slot.rects(&self.ui_rect, item, slot.is_in(&self.cursor_pos)));
             }
 
             // carried item
@@ -244,6 +292,13 @@ impl InventoryMenu {
             }
             
         }
+    }
+
+    /// Update the item in the output slot, depending on the recipe in the
+    /// crafting slot. This only updates the logic, need to call update
+    /// afterwards for the graphics
+    fn update_craft(&mut self) {
+        self.crafting_output_items = CraftingManager::recipe(&self.crafting_items)
     }
 
     fn from_ndc_to_inventory_position(&self, vec: [f32; 2]) -> Option<InventoryPosition> {
